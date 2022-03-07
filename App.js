4704a5d9b35b9595a4aca0956b51f050041a5937
jeeseongmin <peration0422@exntu.com>
2022-03-08 08:34:13 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <Text>weather</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +18,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
   },
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+# React Native Practice with NomadCoder
+
+기본적인 React Native 구동 방법에 대해 익히기 위해 만든 repository입니다.
+
+Link : [Nomad Coder](https://nomadcoders.co/react-native-for-beginners/lobby)
